87291323a0b6f0e609faaaa2f9e00ea72844f1ef
MultivacX <MultivacX@2020.china>
2020-01-01 21:28:39 +0800

    + easy 589. N-ary Tree Preorder Traversal

---
+++ easy/0589. N-ary Tree Preorder Traversal.h
@@ -0,0 +1,41 @@
+// 589. N-ary Tree Preorder Traversal
+
+// Runtime: 88 ms, faster than 60.82% of C++ online submissions for N-ary Tree Preorder Traversal.
+// Memory Usage: 56.7 MB, less than 10.53% of C++ online submissions for N-ary Tree Preorder Traversal.
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> ans;
+        f(root, ans);
+        return ans;
+    }
+    
+    void f(Node* root, vector<int>& ans) {
+        if (!root) return;
+        
+        ans.push_back(root->val);
+        for (auto child : root->children) {
+            f(child, ans);
+        }
+    }
+};
