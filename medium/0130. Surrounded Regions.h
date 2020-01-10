16c034e1529aa43953f7a3545664ff18c4f72bf4
MultivacX <MultivacX@2020.china>
2020-01-10 20:53:21 +0800

    + medium 130. Surrounded Regions

---
+++ medium/0130. Surrounded Regions.h
@@ -0,0 +1,49 @@
+// 130. Surrounded Regions
+
+// Runtime: 20 ms, faster than 99.40% of C++ online submissions for Surrounded Regions.
+// Memory Usage: 17 MB, less than 6.25% of C++ online submissions for Surrounded Regions.
+
+class Solution {
+public:
+    int m;
+    int n;
+    
+    void solve(vector<vector<char>>& board) {
+        m = board.size();
+        if (m == 0) return;
+        n = board[0].size();
+        if (n == 0) return;
+        
+        for (int i = 0; i < m; ++i) {
+            change(board, i, 0, 'O', 'I');
+            change(board, i, n - 1, 'O', 'I');
+        }
+        for (int j = 1; j < n - 1; ++j) {
+            change(board, 0, j, 'O', 'I');
+            change(board, m - 1, j, 'O', 'I');
+        }
+        
+        for (int i = 1; i < m - 1; ++i) {
+            for (int j = 1; j < n - 1; ++j) {
+                change(board, i, j, 'O', 'X');
+            }
+        }
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                change(board, i, j, 'I', 'O');
+            }
+        }
+    }
+    
+    void change(vector<vector<char>>& board, int i, int j, char from, char to) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != from) return;
+        
+        board[i][j] = to;
+        
+        change(board, i - 1, j, from, to);
+        change(board, i + 1, j, from, to);
+        change(board, i, j - 1, from, to);
+        change(board, i, j + 1, from, to);
+    }
+};
