d2287e9fd3a4d6f106c5ee86c540f3efe2969735
MultivacX <MultivacX@2020.china>
2020-01-26 15:33:22 +0800

    + hard 23. Merge k Sorted Lists

---
+++ hard/0023. Merge k Sorted Lists.h
@@ -0,0 +1,48 @@
+// 23. Merge k Sorted Lists
+
+// Runtime: 20 ms, faster than 98.95% of C++ online submissions for Merge k Sorted Lists.
+// Memory Usage: 12.3 MB, less than 5.95% of C++ online submissions for Merge k Sorted Lists.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        if (n == 0) return nullptr;
+        while (n > 1) {
+            int cnt = 0;
+            for (int i = 0; i < n; i += 2) {
+                //cout << i << endl;
+                lists[i / 2] = merge(lists[i], i + 1 < n ? lists[i + 1] : nullptr);
+                cnt += i + 1 < n ? 1 : 0;
+            }
+            n -= cnt;
+        }
+        return lists[0];
+    }
+    
+    ListNode* merge(ListNode* a, ListNode* b) {
+        if (!b) return a;
+        ListNode ans(-1);
+        auto p = &ans;
+        while (a && b) {
+            if (a->val <= b->val) {
+                p->next = a;
+                a = a->next;
+            } else {
+                p->next = b;
+                b = b->next;
+            }
+            p = p->next;
+        }
+        if (p && (a || b)) p->next = a ? a : b;
+        return ans.next;
+    }
+};
