9f41be5cd80f7395a3e579c189855691df7f725e
MultivacX <MultivacX@2020.china>
2020-02-02 16:09:58 +0800

    + medium 380. Insert Delete GetRandom O(1)

--- medium/0380. Insert Delete GetRandom O(1).h
+++ medium/0380. Insert Delete GetRandom O(1).h
@@ -1,59 +1,55 @@
 // 380. Insert Delete GetRandom O(1)
 
-// WRONG
-// 17 / 18 test cases passed.
-/*
+// Runtime: 56 ms, faster than 69.72% of C++ online submissions for Insert Delete GetRandom O(1).
+// Memory Usage: 22.3 MB, less than 76.92% of C++ online submissions for Insert Delete GetRandom O(1).
+
 class RandomizedSet {
 public:
-    // Initialize your data structure here. 
+    /** Initialize your data structure here. */
     RandomizedSet() {
-        std::random_device rd;
-        gen = std::default_random_engine(rd());
+
     }
     
-    // Inserts a value to the set. Returns true if the set did not already contain the specified element. 
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
         if (mem.count(val)) 
             return false;
         
-        mem.insert({val, len});
-        if (len == vals.size())
-            vals.push_back(val);
-        else
-            vals[len] = val;
-        ++len;
+        mem.insert({val, vals.size()});
+        vals.push_back(val);
         
         return true;
     }
     
-    // Removes a value from the set. Returns true if the set contained the specified element. 
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
         if (!mem.count(val)) 
             return false;
-        
+
         int idx = mem[val];
         mem.erase(val);
-        vals[idx] = vals[len - 1];
-        --len;
-        if (len) 
-            mem[vals[idx]] = idx;
+        
+        int last_val = vals.back(); 
+        vals[idx] = last_val;
+        if (idx == vals.size() - 1) {
+            vals.pop_back();
+        } else {
+            vals.pop_back();
+            mem[last_val] = idx;
+        }
         
         return true;
     }
     
-    // Get a random element from the set.
+    /** Get a random element from the set. */
     int getRandom() {
-        std::uniform_int_distribution<int> dis(0, len - 1);
-        return vals[dis(gen)];
+        return vals[rand() % vals.size()];
     }
     
     unordered_map<int, int> mem;
     vector<int> vals;
-    int len{0};
-    
-    std::default_random_engine gen;
 };
-*/
+
 /**
  * Your RandomizedSet object will be instantiated and called as such:
  * RandomizedSet* obj = new RandomizedSet();
