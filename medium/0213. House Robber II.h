ee4fc11dda266ce388091edac0b8b50b42d95cf0
MultivacX <MultivacX@2020.china>
2020-02-18 19:22:32 +0800

    + medium 213. House Robber II

---
+++ medium/0213. House Robber II.h
@@ -0,0 +1,35 @@
+// 213. House Robber II
+
+// Runtime: 4 ms, faster than 59.94% of C++ online submissions for House Robber II.
+// Memory Usage: 8.6 MB, less than 88.57% of C++ online submissions for House Robber II.
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        const int n = nums.size();
+        if (n == 0) return 0;
+        if (n <= 3) return max(nums[n >> 1], max(nums.front(), nums.back()));
+        
+        int ans = 0;
+        int _max = 0;
+        vector<int> dp(n, 0);
+        // dp[i] = nums[i] + max(dp[0, ..., i - 2])
+        
+        for (int i = 0; i < n - 1; ++i) {
+            if (i - 2 >= 0) _max = max(_max, dp[i - 2]);
+            dp[i] = nums[i] + _max;
+            ans = max(ans, dp[i]);
+        }
+        
+        _max = 0;
+        fill(dp.begin(), dp.end(), 0);
+        
+        for (int i = 1; i < n; ++i) {
+            if (i - 2 >= 0) _max = max(_max, dp[i - 2]);
+            dp[i] = nums[i] + _max;
+            ans = max(ans, dp[i]);
+        }
+        
+        return ans;
+    }
+};
