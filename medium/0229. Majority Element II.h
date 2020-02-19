e060220f48212105294630d8bb80b567074832dd
MultivacX <MultivacX@2020.china>
2020-02-19 22:28:17 +0800

    + medium 229. Majority Element II

--- medium/0229. Majority Element II.h
+++ medium/0229. Majority Element II.h
@@ -1,48 +1,45 @@
 // 229. Majority Element II
 
-// WRONG
-/*class Solution {
+// Runtime: 8 ms, faster than 99.53% of C++ online submissions for Majority Element II.
+// Memory Usage: 10.6 MB, less than 73.33% of C++ online submissions for Majority Element II.
+
+class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int cnt1 = 0;
-        int num1 = 0;
+        int num1 = 0; // init value doesn't matter
         int cnt2 = 0;
-        int num2 = 0;
+        int num2 = 0; // init value doesn't matter
         for (int num : nums) {
-            if (cnt1 == 0) {
+            if (num1 == num) {
+                ++cnt1;
+            } else if (num2 == num) {
+                ++cnt2;
+            } else if (cnt1 == 0) {
                 ++cnt1;
                 num1 = num;
-            } else if (num1 == num) {
-                ++cnt1;
             } else if (cnt2 == 0) {
                 ++cnt2;
                 num2 = num;
-            } else if (num2 == num) {
-                ++cnt2;
-            } else if (cnt1 < cnt2) {
-                --cnt1;
             } else {
+                --cnt1;
                 --cnt2;
             }
             
-            printf("%d:{%d}, %d:{%d}\n", cnt1, num1, cnt2, num2);
+            // printf("%d:{%d}, %d:{%d}\n", cnt1, num1, cnt2, num2);
+        }
+        
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int num : nums) {
+            if (num1 == num) ++cnt1;
+            else if (num2 == num) ++cnt2;
         }
         
         const int n = nums.size() / 3;
-        int c1 = 0;
-        int c2 = 0;
         vector<int> ans;
-        for (int num : nums) {
-            if (num1 == num && ++c1 > n) {
-                if (ans.empty() || ans.back() != num) 
-                    ans.push_back(num);
-                c1 = INT_MIN;
-            } else if (num2 == num && ++c2 > n) {
-                if (ans.empty() || ans.back() != num) 
-                    ans.push_back(num);
-                c2 = INT_MIN;
-            }
-        }
+        if (cnt1 > n) ans.push_back(num1);
+        if (cnt2 > n) ans.push_back(num2);
         return ans;
     }
-};*/
+};
