09e25b8bf46b7b45deb6567de22f3967f052486c
MultivacX <MultivacX@2020.china>
2020-02-19 19:07:45 +0800

    + medium 207. Course Schedule

--- medium/0207. Course Schedule.h
+++ medium/0207. Course Schedule.h
@@ -1,37 +1,43 @@
 // 207. Course Schedule
 
+// Runtime: 24 ms, faster than 64.90% of C++ online submissions for Course Schedule.
+// Memory Usage: 14.3 MB, less than 45.45% of C++ online submissions for Course Schedule.
+
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         if (numCourses <= 0 || prerequisites.empty()) return true;
         
-        vector<vector<int>> g(numCourses, vector<int>());
+        vector<unordered_set<int>> graph(numCourses, unordered_set<int>());
         vector<int> degree(numCourses, 0);
         for (const vector<int>& v : prerequisites) {
-            g[v[1]].push_back(v[0]);
+            graph[v[1]].insert(v[0]);
             ++degree[v[0]];
         }
         
+        vector<int> order;
+        deque<int> q;
         for (int i = 0; i < numCourses; ++i) {
-            vector<int> visited(degree);
-            if (isCircle(g, i, visited)) {
-                return false;
+            // printf("%d:%d\n", i, degree[i]);
+            if (degree[i] != 0) continue;
+            q.push_back(i);
+            order.push_back(i);
+        }
+        while (!q.empty()) {
+            int size = q.size();
+            while (size > 0) {
+                int course = q.front();
+                q.pop_front();
+                --size;
+                
+                for (int i : graph[course]) {
+                    --degree[i];
+                    if (degree[i] != 0) continue;
+                    q.push_back(i);
+                    order.push_back(i);
+                }
             }
         }
-        
-        return true;
-    }
-    
-    bool isCircle(const vector<vector<int>>& g, int course, vector<int>& visited) {
-        if (visited[course] < 0) return true;
-        --visited[course];
-        
-        for (int next_course : g[course]) {
-            if (isCircle(g, next_course, visited)) {
-                return true;
-            }
-        }
-        
-        return false;
+        return order.size() == numCourses ? true : false;
     }
 };
