fcd3bfd63ac577d6c130b21600b977f12225a53a
MultivacX <MultivacX@2020.china>
2020-02-20 18:11:30 +0800

    + hard 140. Word Break II

--- hard/0140. Word Break II.h
+++ hard/0140. Word Break II.h
@@ -1,5 +1,53 @@
 // 140. Word Break II
 
+// Runtime: 4 ms, faster than 99.68% of C++ online submissions for Word Break II.
+// Memory Usage: 12.3 MB, less than 87.88% of C++ online submissions for Word Break II.
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        if (s.empty() || wordDict.empty())
+            return {};
+
+        unordered_set<int> lens;
+        for (const string& word : wordDict)
+            lens.insert(word.length());
+        
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+        unordered_map<string, vector<string>> m;
+        
+        return wordBreak(s, lens, words, m);
+    }
+
+    vector<string> wordBreak(string s, 
+                              const unordered_set<int>& lens,
+                              const unordered_set<string>& words,
+                              unordered_map<string, vector<string>>& m) {
+        auto it = m.find(s);
+        if (it != m.end()) return m[s];
+        
+        vector<string> ans;
+        for (int len : lens) {
+            if (len > s.length()) continue;
+            
+            auto word = s.substr(0, len);
+            if (words.count(word) == 0) continue;
+            // cout << s << ": " << word << " >> " << s.substr(len) << endl;
+            if (len == s.length()) {
+                ans.emplace_back(word);
+            } else {
+                for (auto ss : wordBreak(s.substr(len), lens, words, m)) {
+                    // cout << "    " << s << endl;
+                    if (ss.empty()) ans.emplace_back(word);   
+                    else ans.emplace_back(word + " " + ss);   
+                }   
+            }
+        }
+         m[s] = ans;
+        return m[s];
+    }
+};
+
 // Time Limit Exceeded
 // 31 / 39 test cases passed.
 /*class Solution {
