e806c4011aaaeaaf5abcd5168cd75389180187e7
MultivacX <MultivacX@2020.china>
2020-02-22 19:35:23 +0800

    + medium 240. Search a 2D Matrix II

---
+++ medium/0240. Search a 2D Matrix II.h
@@ -0,0 +1,20 @@
+// 240. Search a 2D Matrix II
+
+// Runtime: 292 ms, faster than 12.03% of C++ online submissions for Search a 2D Matrix II.
+// Memory Usage: 12.9 MB, less than 73.33% of C++ online submissions for Search a 2D Matrix II.
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int end = -1;
+        for (const auto& row : matrix) {
+            if (end < 0) end = row.size();
+            auto it = lower_bound(row.begin(), row.begin() + end, target);
+            if (it == row.end()) continue;
+            if (*it == target) return true;
+            if (it == row.begin()) break;
+            end = it - row.begin();
+        }
+        return false;
+    }
+};
