c3ed0cc5b38cfa6dbb4cdfd9d8b01a2693a9c93a
MultivacX <MultivacX@2020.china>
2020-02-29 21:19:12 +0800

    + hard 132. Palindrome Partitioning II

--- hard/0132. Palindrome Partitioning II.h
+++ hard/0132. Palindrome Partitioning II.h
@@ -1,5 +1,36 @@
 // 132. Palindrome Partitioning II
 
+// Runtime: 8 ms, faster than 92.92% of C++ online submissions for Palindrome Partitioning II.
+// Memory Usage: 8.1 MB, less than 100.00% of C++ online submissions for Palindrome Partitioning II.
+
+class Solution {
+public:
+    int minCut(string s) {
+        const int n = s.length();
+        if (n <= 1) return 0;
+        
+        vector<int> cuts(n + 1, 0);
+        for (int i = 0; i <= n; ++i)
+            cuts[i] = i - 1;
+        for (int i = 0; i <= n; ++i) {
+            cout << "odd:" << endl;
+            for (int j = 0; i - j >= 0 && i + j < n && s[i - j] == s[i + j]; ++j) {
+                // cuts[i - j] s[i - j] ... s[i] ... s[i + j] cuts[i + j + 1]
+                cuts[i + j + 1] = min(cuts[i + j + 1], 1 + cuts[i - j]);
+                // printf("s[%d]:%c==s[%d]:%c, ", i - j, s[i - j], i + j, s[i + j]);
+            }
+            
+            cout << "even:" << endl;
+            for (int j = 1; i - j + 1 >= 0 && i + j < n && s[i - j + 1] == s[i + j]; ++j) {
+                // cuts[i - j + 1] s[i - j + 1] ... s[i] s[i + 1] ... s[i + j] cuts[i + j + 1]
+                cuts[i + j + 1] = min(cuts[i + j + 1], 1 + cuts[i - j + 1]);
+                // printf("s[%d]:%c==s[%d]:%c, ", i - j + 1, s[i - j + 1], i + j, s[i + j]);
+            }
+        }
+        return cuts[n];
+    }
+};
+
 // Time Limit Exceeded
 // 26 / 29 test cases passed.
 /*class Solution {
