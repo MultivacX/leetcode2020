a8e77bbcc8357cdfda738bcf0aad23db44ef2ddb
MultivacX <MultivacX@2020.china>
2020-03-19 21:29:58 +0800

    + medium 456. 132 Pattern

---
+++ medium/0456. 132 Pattern.h
@@ -0,0 +1,30 @@
+// 456. 132 Pattern
+
+// Runtime: 40 ms, faster than 36.38% of C++ online submissions for 132 Pattern.
+// Memory Usage: 12.6 MB, less than 25.00% of C++ online submissions for 132 Pattern.
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        const int n = nums.size();
+        if (n < 3) return false;
+        
+        vector<int> min_nums{nums[0]};
+        for (int i = 1; i < n - 2; ++i) 
+            min_nums.push_back(min(min_nums.back(), nums[i]));
+        
+        set<int> s;
+        s.insert(nums[n - 1]);
+        for (int j = n - 2; j > 0; --j) {
+            auto it = s.insert(nums[j]).first;
+            if (it == s.begin()) continue;
+            --it;
+            int ak = *it;
+            int aj = nums[j];
+            int ai = min_nums[j - 1];
+            if (ai < ak && ak < aj) return true;
+        }
+        
+        return false;
+    }
+};
