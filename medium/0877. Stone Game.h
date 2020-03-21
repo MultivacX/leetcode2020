f2be73730eb1fe6fa1566dd834608122d76ad93e
MultivacX <MultivacX@2020.china>
2020-03-21 22:57:16 +0800

    + medium 877. Stone Game

---
+++ medium/0877. Stone Game.h
@@ -0,0 +1,11 @@
+// 877. Stone Game
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Stone Game.
+// Memory Usage: 6.2 MB, less than 100.00% of C++ online submissions for Stone Game.
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
