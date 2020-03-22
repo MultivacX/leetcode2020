7945020ba23129f881a109d062f7f8db09919d23
MultivacX <MultivacX@2020.china>
2020-03-22 21:43:53 +0800

    + hard 862. Shortest Subarray with Sum at Least K

--- hard/0862. Shortest Subarray with Sum at Least K.h
+++ hard/0862. Shortest Subarray with Sum at Least K.h
@@ -1,8 +1,39 @@
 // 862. Shortest Subarray with Sum at Least K
 
+// Runtime: 180 ms, faster than 31.48% of C++ online submissions for Shortest Subarray with Sum at Least K.
+// Memory Usage: 25.2 MB, less than 20.00% of C++ online submissions for Shortest Subarray with Sum at Least K.
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& A, int K) {
+        int len = A.size() + 1;
+        
+        int64_t sum = 0;
+        // q.front < ... < q.back
+        deque<pair<int64_t, int>> q;
+        q.push_back({sum, 0});
+        for (int i = 1; i <= A.size(); ++i) {
+            sum += A[i - 1];
+            
+            while (!q.empty() && sum <= q.back().first) {
+                printf("idx:%d, cur:%d, del:%d,%d\n", i - 1, sum, q.back().first, q.back().second - 1);
+                q.pop_back();
+            }
+            while (!q.empty() && sum >= q.front().first + K) {
+                printf("idx:%d, cur:%d, ***:%d,%d\n", i - 1, sum, q.front().first, q.front().second - 1);
+                len = min(len, i - q.front().second);
+                q.pop_front();
+            }
+            q.push_back({sum, i});
+        }
+        
+        return len == A.size() + 1 ? -1 : len;
+    }
+};
+
 // Time Limit Exceeded
 // 85 / 93 test cases passed.
-class Solution {
+/* class Solution {
 public:
     int shortestSubarray(vector<int>& A, int K) {
         if (A[0] >= K) return 1;
@@ -32,4 +63,4 @@ public:
         }
         return len == A.size() + 1 ? -1 : len;
     }
-};
+}; */
