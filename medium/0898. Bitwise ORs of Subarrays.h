100c3be37daad8d23e0819d56d07a51a095a2b24
MultivacX <MultivacX@2020.china>
2020-03-29 20:50:03 +0800

    + medium 898. Bitwise ORs of Subarrays

--- medium/0898. Bitwise ORs of Subarrays.h
+++ medium/0898. Bitwise ORs of Subarrays.h
@@ -1,5 +1,22 @@
 // 898. Bitwise ORs of Subarrays
 
+// Runtime: 1956 ms, faster than 5.17% of C++ online submissions for Bitwise ORs of Subarrays.
+// Memory Usage: 182.8 MB, less than 100.00% of C++ online submissions for Bitwise ORs of Subarrays.
+
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& A) {
+        unordered_set<int> ans, cur, nxt;
+        for (const int& a : A) {
+            nxt = {a};
+            for (const int& c : cur) nxt.insert(a | c);
+            cur = nxt;
+            for (const int& c : cur) ans.insert(c);
+        }
+        return ans.size();
+    }
+};
+
 // Time Limit Exceeded
 // 83 / 83 test cases passed, but took too long.
 /* class Solution {
