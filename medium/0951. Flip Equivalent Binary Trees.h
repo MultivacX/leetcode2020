0af24a107522ad5d92d10c3192865af4aceb69b7
MultivacX <MultivacX@2020.china>
2020-04-02 11:04:18 +0800

    + medium 951. Flip Equivalent Binary Trees

---
+++ medium/0951. Flip Equivalent Binary Trees.h
@@ -0,0 +1,23 @@
+// 951. Flip Equivalent Binary Trees
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Flip Equivalent Binary Trees.
+// Memory Usage: 9.3 MB, less than 100.00% of C++ online submissions for Flip Equivalent Binary Trees.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (!root1 || !root2) return !root1 && !root2;
+        if (root1->val != root2->val) return false;
+        return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right))
+            || (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+    }
+};
