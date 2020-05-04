1b328e882d7d2fa59d290ac42e9404f843372222
MultivacX <MultivacX@2020.china>
2020-05-05 00:01:41 +0800

    + easy 231. Power of Two

---
+++ easy/0231. Power of Two.h
@@ -0,0 +1,22 @@
+// 231. Power of Two
+
+// Runtime: 4 ms, faster than 34.67% of C++ online submissions for Power of Two.
+// Memory Usage: 5.9 MB, less than 100.00% of C++ online submissions for Power of Two.
+
+// Time Complexity: O(1).
+// Space Complexity: O(1).
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {        
+        if (n > 0) {
+            int i = 0;
+            while (i <= 30 && ((1 << i) & n) == 0) ++i;
+            ++i;
+            while (i <= 30 && ((1 << i) & n) == 0) ++i;
+            return i > 30;
+        }
+        
+        return false;
+    }
+};
