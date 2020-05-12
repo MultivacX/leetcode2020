ea01c2d18fce2b78ee0535d1011a7ff01d208220
MultivacX <MultivacX@2020.china>
2020-05-12 16:06:01 +0800

    + medium 319. Bulb Switcher

---
+++ medium/0319. Bulb Switcher.h
@@ -0,0 +1,11 @@
+// 319. Bulb Switcher
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Bulb Switcher.
+// Memory Usage: 6.2 MB, less than 100.00% of C++ online submissions for Bulb Switcher.
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
