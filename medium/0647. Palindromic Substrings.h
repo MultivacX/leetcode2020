911b8012e26f9c801d6e39331ec00ca0810cdad3
MultivacX <MultivacX@2020.china>
2020-05-14 15:26:51 +0800

    + medium 647. Palindromic Substrings

---
+++ medium/0647. Palindromic Substrings.h
@@ -0,0 +1,21 @@
+// 647. Palindromic Substrings
+
+// Runtime: 4 ms, faster than 98.02% of C++ online submissions for Palindromic Substrings.
+// Memory Usage: 6.4 MB, less than 100.00% of C++ online submissions for Palindromic Substrings.
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); ++i)
+            ans += count(s, i, i) + count(s, i, i + 1);
+        return ans;
+    }
+    
+    int count(const string& s, int i, int j) {
+        const int N = s.length();
+        int ans = 0;
+        while (i >= 0 && j < N && s[i--] == s[j++]) ++ans;
+        return ans;
+    }
+};
