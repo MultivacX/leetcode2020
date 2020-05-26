050b8e0ce65486319c5354690c22f104670b1fa3
MultivacX <MultivacX@2020.china>
2020-05-26 16:40:19 +0800

    + hard 1316. Distinct Echo Substrings

--- hard/1316. Distinct Echo Substrings.h
+++ hard/1316. Distinct Echo Substrings.h
@@ -1,15 +1,36 @@
 // 1316. Distinct Echo Substrings
 
+// Runtime: 784 ms, faster than 42.02% of C++ online submissions for Distinct Echo Substrings.
+// Memory Usage: 558.9 MB, less than 100.00% of C++ online submissions for Distinct Echo Substrings.
+
 class Solution {
 public:
     int distinctEchoSubstrings(string text) {
         const int N = text.length();
         unordered_set<string> subs;
         for (int L = 1; L <= N / 2; ++L) {
-            for (int i = 0; i <= N - L; ++i) {
-                
+            int same = 0;
+            for (int i = 0; i < L; ++i) 
+                if (text[i] == text[i+L])
+                    ++same;
+            
+            // (a  b  c)(a  b  c) a  b  c
+            //  a (b  c  a)(b  c  a)
+            //     +  +     +  +
+            
+            // [i,        i+L-1] [i+L,         i+2L-1]
+            // move right
+            // same([i+1, i+L-1],      [i+L+1, i+2L-1])
+            //      [i+1,         i+L] [i+L+1,         i+2L]
+            
+            for (int i = 0; i <= N - 2 * L; ++i) {
+                if (same == L) subs.insert(text.substr(i, L));
+                if (i >= N - 2 * L) break;
+                if (text[i] == text[i+L]) --same;
+                if (text[i+L] == text[i+2*L]) ++same;
             }
         }
+        // for (auto& s : subs) cout << s << endl;
         return subs.size();
     }
 };
