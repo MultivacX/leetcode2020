e4ade4d025e0033dcd38f87a2de03071c6f7a969
MultivacX <MultivacX@2020.china>
2020-05-27 20:50:58 +0800

    + medium 833. Find And Replace in String

---
+++ medium/0833. Find And Replace in String.h
@@ -0,0 +1,32 @@
+// 833. Find And Replace in String
+
+// Runtime: 8 ms, faster than 65.00% of C++ online submissions for Find And Replace in String.
+// Memory Usage: 11.1 MB, less than 11.11% of C++ online submissions for Find And Replace in String.
+
+class Solution {
+public:
+    string findReplaceString(string S, vector<int>& indexes, vector<string>& sources, vector<string>& targets) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < indexes.size(); ++i) m[indexes[i]] = i;
+        
+        const int L = S.length();
+        string ans;
+        for (int i = 0, j = 0; i < L;) {
+            if (m.count(i)) {
+                int j = m[i];
+                int k = 0;
+                for (; k < sources[j].length(); ++k) 
+                    if (i + k >= L || S[i + k] != sources[j][k]) break;
+                if (k == sources[j].length()) {
+                    ans += targets[j];
+                    i += sources[j].length();
+                } else {
+                    ans += S[i++];
+                }
+            } else {
+                ans += S[i++];
+            }
+        }
+        return ans;
+    }
+};
