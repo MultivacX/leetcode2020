4172d633749bad4c25c3f6f4cf03d0810282c848
MultivacX <MultivacX@2020.china>
2020-06-03 17:39:30 +0800

    + hard 906. Super Palindromes

--- hard/0906. Super Palindromes.h
+++ hard/0906. Super Palindromes.h
@@ -1,19 +1,10 @@
 // 906. Super Palindromes
+// https://leetcode.com/problems/super-palindromes/submissions/
+
+// Runtime: 440 ms, faster than 36.36% of C++ online submissions for Super Palindromes.
+// Memory Usage: 35.8 MB, less than 100.00% of C++ online submissions for Super Palindromes.
 
 class Solution {
-    uint64_t get_palindrome(int& len, uint64_t h, int& hl) {
-        string L(to_string(h));
-        if (L.length() > hl) {
-            L.pop_back();
-            len += 1;
-            hl = (len + 1) >> 1;
-        }
-        string R(L);
-        if (len & 1) R.pop_back();
-        reverse(begin(R), end(R));
-        return stoll(L + R);
-    }
-    
     bool is_palindrome(uint64_t p) {
         string s(to_string(p));
         int i = 0;
@@ -22,25 +13,55 @@ class Solution {
         return true;
     }
     
+    uint64_t get_palindrome(uint64_t v) {
+        string s(to_string(v));
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) s[j--] = s[i++];
+        return stoll(s);
+    }
+    
+    uint64_t palindrome_plus_1(uint64_t v) {
+        string s(to_string(v));
+        const int N = s.length();
+        int k = N / 2;
+        int carry = 1;
+        while (k < N) {
+            int val = carry + s[k] - '0';
+            carry = val / 10;
+            s[k] = val % 10 + '0';
+            ++k;
+        }
+        // cout << v << ", ";
+        if (carry) {
+            string t(N + 1, '0');
+            t[0] = t[N] = '1';
+            // cout << t << endl;
+            return stoll(t);
+        } else {
+            int i = 0;
+            int j = N - 1;
+            while (i < j) s[i++] = s[j--];
+            // cout << s << endl;
+            return stoll(s);
+        }
+    }
+    
 public:
     int superpalindromesInRange(string L, string R) {
-        uint64_t l = stoll(L);
-        uint64_t r = stoll(R);
+        const uint64_t L_ = stoll(L);
+        const uint64_t R_ = stoll(R);
         
-        int len = L.length();
-        int hl = (len + 1) >> 1;
-        uint64_t h = stoll(L.substr(0, hl));
-        
+        uint64_t l = get_palindrome(sqrt(L_));
         int ans = 0;
-        while (true) {
-            uint64_t p = get_palindrome(len, h, hl);
-            if (p > r) break;
-            h += 1;
-            if (p < l) continue;
-            
-            uint64_t s = sqrt(p);
-            if (s * s == p && is_palindrome(s)) ++ans;
-            // cout << p << " " << s << endl;
+        while (l * l <= R_) {
+            // cout << l << ", " << l * l << endl;
+            if (l * l < L_ || !is_palindrome(l * l)) {
+                l = palindrome_plus_1(l);
+                continue;
+            }
+            l = palindrome_plus_1(l);
+            ++ans;
         }
         return ans;
     }
