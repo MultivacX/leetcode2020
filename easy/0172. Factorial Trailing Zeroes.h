586a76c16acb446c6647e56ae21e4341eab1bfcc
MultivacX <MultivacX@2020.china>
2020-06-06 00:34:59 +0800

    + easy 172. Factorial Trailing Zeroes

---
+++ easy/0172. Factorial Trailing Zeroes.h
@@ -0,0 +1,13 @@
+// 172. Factorial Trailing Zeroes
+// https://leetcode.com/problems/factorial-trailing-zeroes/
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Factorial Trailing Zeroes.
+// Memory Usage: 6.1 MB, less than 34.84% of C++ online submissions for Factorial Trailing Zeroes.
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if (n < 5) return 0;
+        return n / 5 + trailingZeroes(n / 5);
+    }
+};
