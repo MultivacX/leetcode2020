8747814a46c6b408bf0fa6bd274bbac559690d95
Jiang Guo <>
2020-10-21 11:53:42 +0800

    + medium 1144. Decrease Elements To Make Array Zigzag

--- medium/1144. Decrease Elements To Make Array Zigzag.h
+++ medium/1144. Decrease Elements To Make Array Zigzag.h
@@ -1,55 +1,21 @@
 // 1144. Decrease Elements To Make Array Zigzag
 // https://leetcode.com/problems/decrease-elements-to-make-array-zigzag/
 
-// misunderstand
-// 20 / 31 test cases passed.
-// Status: Wrong Answer
-/*class Solution {
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Decrease Elements To Make Array Zigzag.
+// Memory Usage: 7.4 MB, less than 25.69% of C++ online submissions for Decrease Elements To Make Array Zigzag.
+
+class Solution {
 public:
     int movesToMakeZigzag(vector<int>& nums) {
         const int N = nums.size();
-        if (N <= 2) return 0;
-        
-        // a[2k-1] < a[2k] > a[2k+1]
-        vector<int> dp(N, 0);
-        dp[0] = nums[0] > nums[1] ? 0 : nums[1] + 1 - nums[0];
-        for (int i = 1; i < N; ++i) {
-            int l = nums[i - 1];
-            int m = nums[i];
-            if (i % 2 == 0) {
-                int r = i == N - 1 ? -1 : nums[i + 1];
-                int maxVal = max(l, r);
-                int moves = m > maxVal ? 0 : maxVal + 1 - m;
-                dp[i] = moves + (i > 1 ? dp[i - 2] : 0);
-            } else {
-                int r = i == N - 1 ? 1001 : nums[i + 1];
-                int minVal = min(l, r);
-                int moves = m < minVal ? 0 : m + 1 - minVal;
-                dp[i] = moves + (i > 1 ? dp[i - 2] : 0);
-            }
+        int e = 0;
+        int o = 0;
+        for (int i = 0; i < N; ++i) {
+            int v = min(i == 0 ? 1001 : nums[i - 1], i == N - 1 ? 1001 : nums[i + 1]);
+            int moves = v > nums[i] ? 0 : (nums[i] - v + 1);
+            if (i % 2 == 0) e += moves;
+            else o += moves;
         }
-        
-        int ans = min(dp[N - 2], dp[N - 1]);
-        
-        // a[2k-1] > a[2k] < a[2k+1]
-        fill(begin(dp), end(dp), 0);
-        dp[0] = nums[0] < nums[1] ? 0 : nums[0] + 1 - nums[1];
-        for (int i = 1; i < N; ++i) {
-            int l = nums[i - 1];
-            int m = nums[i];
-            if (i % 2 == 0) {
-                int r = i == N - 1 ? 1001 : nums[i + 1];
-                int minVal = min(l, r);
-                int moves = m < minVal ? 0 : m + 1 - minVal;
-                dp[i] = moves + (i > 1 ? dp[i - 2] : 0);
-            } else {
-                int r = i == N - 1 ? -1 : nums[i + 1];
-                int maxVal = max(l, r);
-                int moves = m > maxVal ? 0 : maxVal + 1 - m;
-                dp[i] = moves + (i > 1 ? dp[i - 2] : 0);
-            }
-        }
-        
-        return min(ans, min(dp[N - 2], dp[N - 1]));
+        return min(e, o);
     }
-};*/
+};
