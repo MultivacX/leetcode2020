0d74a11658d06d01006ad7d7feca3f523de41a7e
Jiang Guo <>
2020-10-22 23:49:46 +0800

    + easy 35. Search Insert Position

---
+++ easy/0035. Search Insert Position.h
@@ -0,0 +1,9 @@
+// 35. Search Insert Position
+// https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(begin(nums), end(nums), target) - begin(nums);
+    }
+};
