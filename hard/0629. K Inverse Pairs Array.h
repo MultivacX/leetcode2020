e56a3af965b2541a2ae77f2a744530a051c94255
X <>
2020-10-30 20:32:44 +0800

    + hard 629. K Inverse Pairs Array

--- hard/0629. K Inverse Pairs Array.h
+++ hard/0629. K Inverse Pairs Array.h
@@ -1,4 +1,61 @@
 // 629. K Inverse Pairs Array
+// https://leetcode.com/problems/k-inverse-pairs-array/submissions/
+
+// Runtime: 44 ms, faster than 34.57% of C++ online submissions for K Inverse Pairs Array.
+// Memory Usage: 16.1 MB, less than 9.26% of C++ online submissions for K Inverse Pairs Array.
+
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        if (k == 0) return 1;
+        
+        vector<int> dp(k + 1, 0);
+        for (int i = 1; i <= n; ++i) {
+            vector<int> prefix(k + 1, 0);
+            prefix[0] = 1;
+            for (int j = 1; j <= k; ++j)
+                prefix[j] = (prefix[j - 1] + dp[j]) % 1000000007;
+            
+            int m = min(k, (i - 1) * i / 2);
+            for (int j = 1; j <= m; ++j) {
+                int x = max(0, j - (i - 1)) - 1;
+                dp[j] = (prefix[j] - (x >= 0 ? prefix[x] : 0) + 1000000007) % 1000000007;
+            }
+        }
+        return dp[k];
+    }
+};
+
+// Runtime: 56 ms, faster than 24.38% of C++ online submissions for K Inverse Pairs Array.
+// Memory Usage: 44.1 MB, less than 9.26% of C++ online submissions for K Inverse Pairs Array.
+
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        vector<vector<int64_t>> dp(n + 1, vector<int64_t>(k + 1, 0));
+        
+        for (int i = 1; i <= n; ++i) dp[i][0] = 1;
+        
+        for (int i = 1; i <= n; ++i) {
+            vector<int64_t> prefix(k + 1, 0);
+            prefix[0] = dp[i - 1][0];
+            for (int j = 1; j <= k; ++j)
+                prefix[j] = (prefix[j - 1] + dp[i - 1][j]) % 1000000007;
+            
+            for (int j = 1; j <= k && j <= (i - 1) * i / 2; ++j) {
+                /*for (int x = max(0, j - (i - 1)); x <= j; ++x) {
+                    // insert number i before position x
+                    // then get j - x inverse pairs
+                    dp[i][j] += dp[i - 1][x];
+                    dp[i][j] %= 1000000007;
+                }*/
+                int x = max(0, j - (i - 1)) - 1;
+                dp[i][j] = (prefix[j] - (x >= 0 ? prefix[x] : 0) + 1000000007) % 1000000007;
+            }
+        }
+        return dp[n][k];
+    }
+};
 
 // Time Limit Exceeded
 // 35 / 80 test cases passed.
--- medium/0673. Number of Longest Increasing Subsequence.h
+++ medium/0673. Number of Longest Increasing Subsequence.h
@@ -31,3 +31,38 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        const int N = nums.size();
+        if (N <= 1) return N;
+        
+        // {cnt, len}
+        vector<vector<int>> dp(N, vector<int>(2));
+        for (int i = 0; i < N; ++i) {
+            dp[i][0] = 1;
+            dp[i][1] = 1;
+            for (int j = i - 1; j >= 0; --j) {
+                if (nums[j] >= nums[i]) continue;
+                
+                if (dp[j][1] + 1 > dp[i][1]) {
+                    dp[i][0] = dp[j][0];
+                    dp[i][1] = dp[j][1] + 1;
+                } else if (dp[j][1] + 1 == dp[i][1]) {
+                    dp[i][0] += dp[j][0];
+                }
+            }
+            // cout << dp[i][1] << "*" << dp[i][0] << " ";
+        }
+        int maxLen = 1;
+        for (int i = 0; i < N; ++i)
+            if (maxLen < dp[i][1])
+                maxLen = dp[i][1];
+        int ans = 0;
+        for (int i = 0; i < N; ++i)
+            if (maxLen == dp[i][1])
+                ans += dp[i][0];
+        return ans;
+    }
+};
