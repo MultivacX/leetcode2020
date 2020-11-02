2928af10710de56c4371cee4568155c51b1904ad
X <>
2020-11-02 17:18:57 +0800

    + hard 135. Candy

--- hard/0135. Candy.h
+++ hard/0135. Candy.h
@@ -1,28 +1,24 @@
 // 135. Candy
+// https://leetcode.com/problems/candy/
+
+// Runtime: 40 ms, faster than 67.24% of C++ online submissions for Candy.
+// Memory Usage: 18.1 MB, less than 5.95% of C++ online submissions for Candy.
 
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         const int N = ratings.size();
         vector<int> candies(N, 1);
-        int ans = N;
-        for (int i = 0; i < N; ++i) {
-            int cnt = candies[i];
-            if (i - 1 >= 0 && ratings[i - 1] < ratings[i] && candies[i - 1] >= candies[i]) {
-                cnt = candies[i - 1] + 1;
-                ans += cnt - candies[i];
-            }
-            candies[i] = cnt;
-            
-            int j = i;
-            while (j - 1 >= 0 && ratings[j - 1] > ratings[j] && candies[j - 1] <= candies[j]) {
-                candies[j - 1] = candies[j] + 1;
-                ++ans;
-                --j;
+        for (int i = 1; i < N; ++i) {
+            if (ratings[i - 1] < ratings[i] && candies[i - 1] >= candies[i]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
-        // for (int i : ratings) cout << i << " "; cout << endl;
-        // for (int i : candies) cout << i << " "; cout << endl;
-        return ans;
+        for (int i = N - 2; i >= 0; --i) {
+            if (ratings[i + 1] < ratings[i] && candies[i + 1] >= candies[i]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+        return accumulate(begin(candies), end(candies), 0);
     }
 };
