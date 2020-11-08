b73deb10607415271a6910d4d5fdbf3840fd19eb
X <>
2020-11-08 21:57:31 +0800

    + hard 330. Patching Array

--- hard/0330. Patching Array.h
+++ hard/0330. Patching Array.h
@@ -1,53 +1,26 @@
 // 330. Patching Array
 // https://leetcode.com/problems/patching-array/
 
+// Runtime: 8 ms, faster than 96.05% of C++ online submissions for Patching Array.
+// Memory Usage: 11.7 MB, less than 79.84% of C++ online submissions for Patching Array.
+
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
-        vector<bool> arr(n + 1, false);
-        int cnt = 0;
-        int max_val = 0;
-        
-        for (int i : nums) {
-            if (!arr[i]) ++cnt;
-            max_val = max(max_val, i);
-            
-            // i + j <= n
-            for (int j = min(max_val, n - i); j > 0 && cnt < n; --j) {
-                if (!arr[j]) continue;
-                int k = i + j;
-                if (!arr[k]) ++cnt;
-                max_val = max(max_val, k);
-                arr[k] = true;
+        int patches = 0;
+        int64_t exclusive = 1;
+        int i = 0;
+        // [0, exclusive) formed
+        // try to form exclusive
+        while (exclusive <= n) {
+            // use nums[i] to form exclusive
+            if (i < nums.size() && exclusive >= (int64_t)nums[i]) {
+                exclusive += nums[i++];
+            } else {
+                ++patches;
+                exclusive += exclusive;
             }
-            
-            arr[i] = true;
         }
-        
-        // for (int k = 1; k <= n; ++k) cout << (arr[k] ? k : 0) << " "; cout << endl;
-        
-        int ans = 0;
-        
-        for (int i = 1; i <= n && cnt < n; ++i) {
-            if (arr[i]) continue;
-            max_val = max(max_val, i);
-            
-            // i + j <= n
-            for (int j = min(max_val, n - i); j > 0 && cnt < n; --j) {
-                if (!arr[j]) continue;
-                int k = i + j;
-                if (!arr[k]) ++cnt;
-                max_val = max(max_val, k);
-                arr[k] = true;
-            }
-            
-            arr[i] = true;
-            ++ans;
-            
-            // cout << i << " ---------- " << endl;
-            // for (int k = 1; k <= n; ++k) cout << (arr[k] ? k : 0) << " "; cout << endl;
-        }
-
-        return ans;
+        return patches;
     }
 };
--- hard/0715. Range Module.h
+++ hard/0715. Range Module.h
@@ -1,5 +1,73 @@
 // 715. Range Module
 
+class RangeModule {
+    map<int, bool> m;
+    
+public:
+    RangeModule() {
+        
+    }
+    
+    void addRange(int left, int right) {
+        m[left] = true;
+        if (!m.count(right)) m[right] = false;
+        
+        int L = left;
+        auto it = m.find(left);
+        if (it != m.begin() && (--it)->second) L = it->first;
+        
+        int R = right;
+        it = m.find(right);
+        if (it != m.end() && !(++it)->second) R = it->first;
+        
+        vector<int> useless;
+        it = m.find(L); ++it;
+        for (; it != m.find(R); ++it) useless.push_back(it->first);
+        while (!useless.empty()) {
+            m.erase(useless.back());
+            useless.pop_back();
+        }
+    }
+    
+    bool queryRange(int left, int right) {
+        auto it = m.lower_bound(left);
+        if (it == m.end()) return false;
+        if (it == m.begin() && (it->first != left || !it->second)) return false;
+        if (it->first != left && !(--it)->second) return false;
+        auto L = it;
+        
+        auto R = m.lower_bound(right);
+        if (R == m.end()) return false;
+        
+        // L <= left < right <= R
+        
+        int end = 0;
+        for (auto it = L; it != R; ++it) {
+            if (!it->second) ++end;
+            if (end > 0) return false;
+        }
+        return true;
+    }
+    
+    void removeRange(int left, int right) {
+        auto it = m.lower_bound(left);
+        if (it == m.end()) return;
+        if (it != m.begin() && it->first != left) --it;
+        auto L = it;
+        
+        auto R = m.lower_bound(right);
+    }
+};
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
+
+
 // ["RangeModule","addRange","queryRange","addRange","queryRange","addRange","removeRange","removeRange","removeRange","queryRange"]
 // [[],[5,7],[2,7],[6,9],[2,9],[2,7],[3,10],[1,8],[1,10],[4,7]]
 class RangeModule {
