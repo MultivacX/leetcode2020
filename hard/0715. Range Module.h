1e0c7b19aca76d9992dbbff184758fab9c272425
X <>
2020-11-09 11:45:02 +0800

    + hard 715. Range Module

--- hard/0715. Range Module.h
+++ hard/0715. Range Module.h
@@ -1,6 +1,11 @@
 // 715. Range Module
+// https://leetcode.com/problems/range-module/
+
+// Runtime: 312 ms, faster than 60.40% of C++ online submissions for Range Module.
+// Memory Usage: 72.8 MB, less than 5.86% of C++ online submissions for Range Module.
 
 class RangeModule {
+    // {point, start/end}
     map<int, bool> m;
     
 public:
@@ -12,29 +17,32 @@ public:
         m[left] = true;
         if (!m.count(right)) m[right] = false;
         
-        int L = left;
         auto it = m.find(left);
-        if (it != m.begin() && (--it)->second) L = it->first;
+        auto L = it;
+        if (it != m.begin() && (--it)->second) --L;
+        ++L;
         
-        int R = right;
         it = m.find(right);
-        if (it != m.end() && !(++it)->second) R = it->first;
+        auto R = it;
+        ++it;
+        if (it != m.end() && !it->second) ++R;
         
-        vector<int> useless;
-        it = m.find(L); ++it;
-        for (; it != m.find(R); ++it) useless.push_back(it->first);
-        while (!useless.empty()) {
-            m.erase(useless.back());
-            useless.pop_back();
-        }
+        m.erase(L, R);
+        
+        // for (auto it : m) 
+        //     if (it.second) cout << "[" << it.first << ", ";
+        //     else cout << it.first << ")  ";
+        // cout << endl;
     }
     
     bool queryRange(int left, int right) {
-        auto it = m.lower_bound(left);
-        if (it == m.end()) return false;
-        if (it == m.begin() && (it->first != left || !it->second)) return false;
-        if (it->first != left && !(--it)->second) return false;
-        auto L = it;
+        // L <= left
+        auto L = m.lower_bound(left);
+        // not found
+        if (L == m.end()) return false;
+        // L < left or L is end
+        if (L == m.begin() && (L->first != left || !L->second)) return false;
+        if (L->first != left && !(--L)->second) return false;
         
         auto R = m.lower_bound(right);
         if (R == m.end()) return false;
@@ -52,125 +60,25 @@ public:
     void removeRange(int left, int right) {
         auto it = m.lower_bound(left);
         if (it == m.end()) return;
-        if (it != m.begin() && it->first != left) --it;
-        auto L = it;
-        
-        auto R = m.lower_bound(right);
-    }
-};
-
-/**
- * Your RangeModule object will be instantiated and called as such:
- * RangeModule* obj = new RangeModule();
- * obj->addRange(left,right);
- * bool param_2 = obj->queryRange(left,right);
- * obj->removeRange(left,right);
- */
-
-
-// ["RangeModule","addRange","queryRange","addRange","queryRange","addRange","removeRange","removeRange","removeRange","queryRange"]
-// [[],[5,7],[2,7],[6,9],[2,9],[2,7],[3,10],[1,8],[1,10],[4,7]]
-class RangeModule {
-    // [left, right)
-    map<int, int> m;
-    
-public:
-    RangeModule() {
-        
-    }
-    
-    void addRange(int left, int right) {
-        if (m.empty()) {
-            m.emplace(left, right);
-            return;
-        }
-        
-        // right <= it.left
-        auto it = m.lower_bound(right);
-        if (it == m.begin()) {
-            int L = it->first;
-            int R = it->second;
-            if (right < L) m.emplace(left, right);
-            else emplace(left, right);
-            return;
-        }
-        
-        vector<int> removed;
-        --it;
-        while (true) {
-            int L = it->first;
-            int R = it->second;
-            
-            if (R <= left) {
-                break;
-            } else if (L <= left/* and left < R */) {
-                left = L;
-                removed.emplace_back(L);
-                break;
-            } else /* left < L */ {
-                removed.emplace_back(L);
-            }
-            
-            if (it == m.begin())
-                break;
+        if (it != m.begin()) {
             --it;
-        }
-        for (int L : removed) m.erase(L);
-        emplace(left, right);
-    }
-    
-    void emplace(int left, int right) {
-        printf("[%d, %d) ", left, right); cout << " + ";
-        for (auto p : m) printf("[%d, %d) ", p.first, p.second); cout << " -> ";
-        auto it = ++m.emplace(left, right).first;
-        vector<int> removed;
-        while (it != m.end()) {
-            int L = it->first;
-            int R = it->second;
-            if (L <= right) {
-                removed.emplace_back(L);
-                right = max(right, R);
-                m[left] = right;
-            } else {
-                break;
+            if (it->second) {
+                m[left] = false;
+                it = m.find(left);
             }
             ++it;
         }
-        for (int L : removed) m.erase(L);
-        for (auto p : m) printf("[%d, %d) ", p.first, p.second); cout << endl;
-    }
-    
-    bool queryRange(int left, int right) {
-        if (m.empty()) return false;
         
-        // right <= it.left
-        auto it = m.lower_bound(right);
-        if (it == m.begin()) return false;
+        auto L = it;
+        auto R = m.upper_bound(right);
+        bool isRightStart = R != m.end() && !R->second;
+        m.erase(L, R);
+        if (isRightStart) m[right] = true;
         
-        --it;
-        int L = it->first;
-        int R = it->second;
-        return L <= left && right <= R;
-    }
-    
-    void removeRange(int left, int right) {
-        if (m.empty()) return;
-        
-        // right <= it.left
-        auto it = m.lower_bound(right);
-        if (it == m.begin()) return;
-        
-        printf("[%d, %d) ", left, right); cout << " - ";
-        for (auto p : m) printf("[%d, %d) ", p.first, p.second); cout << " -> ";
-        --it;
-        int L = it->first;
-        int R = it->second;
-        if (L <= left && right <= R) {
-            if (L < left) it->second = left;
-            else /* L == left */ m.erase(L);
-            if (right < R) m.emplace(right, R);
-        }
-        for (auto p : m) printf("[%d, %d) ", p.first, p.second); cout << endl;
+        // for (auto it : m) 
+        //     if (it.second) cout << "[" << it.first << ", ";
+        //     else cout << it.first << ")  ";
+        // cout << endl;
     }
 };
 
