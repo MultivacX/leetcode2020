2c5ee3cc5ccb8d2ec315386cbb29af479ddc0a55
X <>
2020-11-13 12:29:41 +0800

    + medium 909. Snakes and Ladders

--- medium/0909. Snakes and Ladders.h
+++ medium/0909. Snakes and Ladders.h
@@ -1,49 +1,51 @@
 // 909. Snakes and Ladders
 // https://leetcode.com/problems/snakes-and-ladders/
 
+// Runtime: 28 ms, faster than 88.96% of C++ online submissions for Snakes and Ladders.
+// Memory Usage: 13.3 MB, less than 60.91% of C++ online submissions for Snakes and Ladders.
+
 class Solution {
-    void move(vector<vector<int>>& board, vector<int> dp, int i, int j) {
-
-    }
-
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         const int N = board.size();
-        vector<int> A(N * N + 1);
-        // vector<vector<int>> B(N, vector<int>(N));
-        for (int i = N - 1, l2r = 1, k = 1; i >= 0; --i) {
-            if (l2r) {
-                for (int j = 0; j < N; ++j) {
-                    A[k] = i * N + j;
-                    // B[i][j] = k;
-                    ++k;
-                }
-            } else {
-                for (int j = N - 1; j >= 0; --j) {
-                    A[k] = i * N + j;
-                    // B[i][j] = k;
-                    ++k;
+        
+        auto getK = [=](int r, int c){
+            return 1 + (N - 1 - r) * N + (((N - r) & 1) ? c : (N - 1 - c));  
+        };
+        auto getRC = [=](int k, int& r, int& c){
+            --k;
+            r = N - 1 - k / N;
+            c = ((N - r) & 1) ? k % N : N - 1 - k % N;
+        };
+        
+        // for (int i = 0; i < N; ++i) {
+        //     for (int j = 0; j < N; ++j) {
+        //         int k, r, c;
+        //         k = getK(i, j);
+        //         getRC(k, r, c);
+        //         printf("%d(%d:%d,%d:%d) ", k, i, r, j, c);
+        //     }
+        //     cout << endl;    
+        // }
+        
+        vector<int> M(N * N + 1, INT_MAX);
+        M[0] = M[1] = 0;
+        int moves = 0;
+        queue<int> q; q.push(1);
+        while (!q.empty()) {
+            ++moves;
+            int size = q.size();
+            while (size-- > 0) {
+                int x = q.front(); q.pop();
+                for (int i = x + 1; i <= x + 6 && i <= N * N; ++i) {
+                    int k = i, r, c; getRC(k, r, c);
+                    if (board[r][c] != -1) k = board[r][c];
+                    if (M[k] <= moves) continue;
+                    M[k] = moves;
+                    q.push(k);
                 }
             }
-            l2r ^= 1;
         }
-        // for (int i = 0; i < N; ++i) {
-        //     for (int j = 0; j < N; ++j)
-        //         printf("%3d ", B[i][j]);
-        //     cout << endl;
-        // }
-        vector<int> dp(N * N + 1, INT_MAX);
-        dp[0] = dp[1] = 0;
-        for (int i = 2, step = 1; i <= N * N; i += 6, ++step) 
-            for (int x = i; x <= i + 6 && x <= N * N; ++x) 
-                dp[x] = step;
-        // for (int i = 0; i < N; ++i) {
-        //     for (int j = 0; j < N; ++j)
-        //         printf("%3d ", dp[B[i][j]]);
-        //     cout << endl;
-        // }
-        
-        
-        return dp[N * N];
+        return M.back() == INT_MAX ? -1 : M.back();
     }
 };
