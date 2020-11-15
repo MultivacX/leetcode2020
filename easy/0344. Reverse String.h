e4baecf1b10eaf4f1c6601f89ffba7d62e74b4cb
X <>
2020-11-15 22:09:44 +0800

    + easy 344. Reverse String

---
+++ easy/0344. Reverse String.h
@@ -0,0 +1,15 @@
+// 344. Reverse String
+// https://leetcode.com/problems/reverse-string/
+
+// Runtime: 32 ms, faster than 79.54% of C++ online submissions for Reverse String.
+// Memory Usage: 23.5 MB, less than 37.15% of C++ online submissions for Reverse String.
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        const int N = s.size();
+        int i = 0; 
+        int j = N - 1;
+        while (i < j) swap(s[i++], s[j--]);
+    }
+};
--- hard/0004. Median of Two Sorted Arrays.h
+++ hard/0004. Median of Two Sorted Arrays.h
@@ -6,16 +6,26 @@ public:
         if (nums1.size() > nums2.size()) swap(nums1, nums2);
         const int M = nums1.size();
         const int N = nums2.size();
+        const int L = M + N - 1;
+        const int K = L / 2;
+        // L is odd : nums[L/2] 
+        // L is even : (nums[L/2] + nums[L/2+1])/2
+        // i + j + 1 == K
         
         int min_i = 0;
         int max_i = M;
-        while (min_i < max_i) {
-            int i = min_i + (max_i - min_i) / 2;
-            // int j = (M + N) / 2 - i - 1;
-
-            if (i == 0) {
-
-            } else if (i == M - 1) {
+        while (true) {
+            if (min_i < max_i) {
+                int i = min_i + (max_i - min_i) / 2;
+                int j = K - i - 1;
+                if (j < 0) {}
+                else if (j >= N) {} 
+                else {
+                    
+                }
+            } else if (max_i == 0) {
+                 
+            } else {
 
             }
         }
