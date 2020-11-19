98b9eeb3a915b8bcd808c58060722d651acbd0a2
X <>
2020-11-19 13:46:50 +0800

    + medium 1654. Minimum Jumps to Reach Home

--- medium/1654. Minimum Jumps to Reach Home.h
+++ medium/1654. Minimum Jumps to Reach Home.h
@@ -1,45 +1,46 @@
 // 1654. Minimum Jumps to Reach Home
 // https://leetcode.com/problems/minimum-jumps-to-reach-home/
 
+// Runtime: 128 ms, faster than 36.54% of C++ online submissions for Minimum Jumps to Reach Home.
+// Memory Usage: 25.2 MB, less than 33.49% of C++ online submissions for Minimum Jumps to Reach Home.
+
 class Solution {
 public:
 	int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-		unordered_set<int> forbi(forbidden.begin(), forbidden.end());
-		vector<vector<int>> visited(2,vector<int>(5000));
-		// visited array has two states:
-		// visited[0][i] means the ith cell was reached from its left hand side jumping forward, 
-		// visited[1][i] means the ith cell was reached from its right hand side jumping backward. 
-		queue<pair<int,bool>> qu; 
-		// true means your last jump was backward jumping, you cannot jump backward in current step.
-		// false, otherwise.
-		qu.push({0,false});
-		visited[0][0] = 1;
-		visited[1][0] = 1;
-		int ans = 0;
-		while(!qu.empty()) {
-			int len = qu.size();
-			while(len > 0) {
-				len--;
-				int cur = qu.front().first;
-				bool flag = qu.front().second;
-                cout << cur << ":" << flag << ", ";
-				if(cur == x) {
-					return ans;
-				}
-				qu.pop();
-				int forward = cur + a;
-				int backward = cur - b;
-				if(forward < 5000 && visited[0][forward] == 0 && !forbi.count(forward)) {
-					qu.push({forward,false});
-					visited[0][forward] = 1;
-				}
-				if(backward >=0 && visited[1][backward] == 0 && !forbi.count(backward) && !flag) {
-					qu.push({backward,true});
-					visited[1][backward] = 1;
-				}
-			}
-            cout << endl;
-			ans++;
+		unordered_set<int> forbidden_(forbidden.begin(), forbidden.end());
+
+        unordered_set<int> forwards{0};
+        unordered_set<int> backwards{0};
+
+        int steps = 0;
+        queue<int> q;
+        q.push(0);
+		while(!q.empty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int position = q.front() >> 1;
+                if (position == x) return steps;
+
+                bool backward = q.front() & 1;
+                q.pop();
+
+                // cout << position << ":" << backward << ", ";
+
+                int fx = position + a;
+                if (fx - b > x && a >= b) {}
+                else if (fx <= 10000 && !forbidden_.count(fx) && !forwards.count(fx)) {
+                    q.push(fx << 1);
+                    forwards.insert(fx);
+                }
+
+                int bx = position - b;
+                if (!backward && bx > 0 && !forbidden_.count(bx) && !backwards.count(bx)) {
+                    q.push((bx << 1) | 1);
+                    backwards.insert(bx);
+                }
+            }
+            // cout << endl;
+            ++steps;
 		}
 		return -1;
 	}
