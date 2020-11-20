8a230d31ee4f138fcbe8d529e146d844eb9d2951
X <>
2020-11-20 16:13:53 +0800

    + hard 828. Count Unique Characters of All Substrings of a Given String

--- hard/0828. Count Unique Characters of All Substrings of a Given String.h
+++ hard/0828. Count Unique Characters of All Substrings of a Given String.h
@@ -1,6 +1,29 @@
 // 828. Count Unique Characters of All Substrings of a Given String
 // https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/
 
+// Runtime: 12 ms, faster than 66.99% of C++ online submissions for Count Unique Characters of All Substrings of a Given String.
+// Memory Usage: 10.4 MB, less than 14.12% of C++ online submissions for Count Unique Characters of All Substrings of a Given String.
+
+class Solution {
+public:
+    int uniqueLetterString(string s) {
+        const int N = s.length();
+        vector<vector<int>> n(26, vector<int>{-1});
+        for (int i = 0; i < N; ++i) 
+            n[s[i] - 'A'].push_back(i);
+        for (int i = 0; i < 26; ++i) 
+            n[i].push_back(N);
+        int64_t ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            auto& v = n[i];
+            const int L = v.size();
+            for (int j = 1; j <= L - 2; ++j) 
+                ans = (ans + (int64_t)(v[j] - v[j - 1]) * int64_t(v[j + 1] - v[j])) % 1000000007;
+        }
+        return ans;
+    }
+};
+
 // TLE
 /*class Solution {
 public:
