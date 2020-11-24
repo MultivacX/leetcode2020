3db7a0ed5aac8fb1b03f1ad724b310079b1031a5
MultivacX <MultivacX@2020.china>
2020-11-24 12:25:03 +0800

    + medium 947. Most Stones Removed with Same Row or Column

--- medium/0947. Most Stones Removed with Same Row or Column.h
+++ medium/0947. Most Stones Removed with Same Row or Column.h
@@ -1,100 +1,39 @@
 // 947. Most Stones Removed with Same Row or Column
 // https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
 
+// Runtime: 52 ms, faster than 96.64% of C++ online submissions for Most Stones Removed with Same Row or Column.
+// Memory Usage: 21.8 MB, less than 34.01% of C++ online submissions for Most Stones Removed with Same Row or Column.
+
 class Solution {
+    int find(vector<int>& parents, int i) {
+        if (parents[i] == i) return i;
+        return find(parents, parents[i]);
+    }
+    
+    void uni(vector<int>& parents, int i, int j) {
+        parents[find(parents, i)] = parents[find(parents, j)];
+    }
+    
 public:
-    int removeStones(vector<vector<int>>& stones) {        
-        const int M = 10000;
-        const int N = stones.size();
+    int removeStones(vector<vector<int>>& stones) {
+        if (stones.size() <= 1) return 0;
         
-        unordered_map<int, unordered_set<int>> x2y, y2x;
-        unordered_map<int, unordered_map<int, int>> xy2i;
-        for (int i = 0; i < N; ++i) {
-            int x = stones[i][0];
-            int y = stones[i][1];
-            x2y[x].insert(y);
-            y2x[y].insert(x);
-            xy2i[x][y] = i;
-        }
+        vector<int> parents(20000);
+        for (int i = 0; i < 20000; ++i)
+            parents[i] = i;
+        for (const auto& s : stones)
+            uni(parents, s[0], s[1] + 10000);
         
-        vector<int> uf(N, -1);
-        for (int i = 0; i < N; ++i) {
-            int k = uf[i] >= 0 ? uf[i] : i;
-            int x = stones[i][0];
-            int y = stones[i][1];
-
-            for (int y : x2y[x]) if (uf[xy2i[x][y]] >= 0 && uf[xy2i[x][y]] < k) k = uf[xy2i[x][y]];
-            for (int x : y2x[y]) if (uf[xy2i[x][y]] >= 0 && uf[xy2i[x][y]] < k) k = uf[xy2i[x][y]];
-
-            for (int y : x2y[x]) uf[xy2i[x][y]] = k;
-            for (int x : y2x[y]) uf[xy2i[x][y]] = k;
-
-            uf[i] = k;
-        }
+        // for (const auto& s : stones) 
+        //     printf("(%d,%d) px[%d]=%d py[%d+1W]=%d\n", s[0], s[1], s[0], parents[s[0]], s[1], parents[s[1]+10000]);
+        // for (const auto& s : stones) 
+        //     printf("find(%d)=%d find(%d+1W)=%d\n", s[0], find(parents, s[0]), s[1], find(parents, s[1]+10000));
         
-        vector<int> n(N, 0);
-        for (int i = 0; i < N; ++i) ++n[uf[i]];
-        
-        // for (int i = 0; i < N; ++i) 
-        //     printf("%d:(%d,%d) -> %d:(%d,%d)\n", 
-        //            i, stones[i][0], stones[i][1], 
-        //            uf[i], stones[uf[i]][0], stones[uf[i]][1]);
-        // cout << endl;
-        // for (int i = 0; i < N; ++i) cout << n[i] << ", "; cout << endl;
+        unordered_map<int, int> m;
+        for (const auto& s : stones) ++m[find(parents, s[0])];
         
         int ans = 0;
-        for (int i = 0; i < N; ++i) if (n[i] > 1) ans += n[i] - 1;
+        for (auto it : m) ans += it.second - 1;
         return ans;
     }
 };
-
-// TLE
-/*class Solution {
-public:
-    int removeStones(vector<vector<int>>& stones) {        
-        const int N = stones.size();
-        vector<int> uf(N, -1);
-        for (int i = 0; i < N; ++i) {
-            if (uf[i] >= 0) continue;
-            
-            uf[i] = i;
-            
-            unordered_set<int> idx;
-            for (int j = i + 1; j < N; ++j) {
-                if (uf[j] >= 0) continue;
-                idx.insert(j);
-            }
-            
-            unordered_set<int> x_pos{stones[i][0]}, y_pos{stones[i][1]};
-            while (!idx.empty()) {
-                int size = idx.size();
-                for (int j : idx) {
-                    int x = stones[j][0];
-                    int y = stones[j][1];
-                    if (x_pos.count(x) || y_pos.count(y)) {
-                        x_pos.insert(x);
-                        y_pos.insert(y);
-                        uf[j] = i;
-                        idx.erase(j);
-                        break;
-                    }
-                }
-                if (size == idx.size()) break;
-            }
-        }
-        
-        vector<int> n(N, 0);
-        for (int i = 0; i < N; ++i) ++n[uf[i]];
-        
-        // for (int i = 0; i < N; ++i) 
-        //     printf("%d:(%d,%d) -> %d:(%d,%d)\n", 
-        //            i, stones[i][0], stones[i][1], 
-        //            uf[i], stones[uf[i]][0], stones[uf[i]][1]);
-        // cout << endl;
-        // for (int i = 0; i < N; ++i) cout << n[i] << ", "; cout << endl;
-        
-        int ans = 0;
-        for (int i = 0; i < N; ++i) if (n[i] > 1) ans += n[i] - 1;
-        return ans;
-    }
-};*/
