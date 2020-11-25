b994c18c6bcecf7bc6b7c9252cd459d45bf23ead
MultivacX <MultivacX@2020.china>
2020-11-25 18:04:26 +0800

    + medium 1536. Minimum Swaps to Arrange a Binary Grid

--- medium/1536. Minimum Swaps to Arrange a Binary Grid.h
+++ medium/1536. Minimum Swaps to Arrange a Binary Grid.h
@@ -1,50 +1,41 @@
 // 1536. Minimum Swaps to Arrange a Binary Grid
 // https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid/
 
-// list ?
-// 1 pos, min_idx after bubble pop
-
+// Runtime: 132 ms, faster than 60.69% of C++ online submissions for Minimum Swaps to Arrange a Binary Grid.
+// Memory Usage: 26.1 MB, less than 42.20% of C++ online submissions for Minimum Swaps to Arrange a Binary Grid.
+    
 class Solution {
 public:
     int minSwaps(vector<vector<int>>& grid) {
         const int N = grid.size();
-        // vector<int> rows(N, -1);
-        // {pos_1, cnt-1}
-        list<pair<int, int>> l;
-        unordered_map<int, int> m;
+        vector<int> zeros(N, 0);
         for (int i = 0; i < N; ++i) {
             for (int j = N - 1; j >= 0; --j) {
-                if (grid[i][j] == 1) {
-                    // rows[i] = j;
-                    l.push_back({j, m[j]++});
-                    break;
-                }
+                if (grid[i][j] == 1) break;
+                ++zeros[i];
             }
         }
-        for (int k : rows) cout << k << " "; cout << endl; cout << endl;
-        vector<int> sortedRows(rows);
-        sort(begin(sortedRows), end(sortedRows));
-        for (int i = 0; i < N; ++i) if (sortedRows[i] > i) return -1;
+        
+        // for (int i = 0; i < N; ++i) cout << i << ":" << zeros[i] << ", "; cout << endl; 
         
         int ans = 0;
-        int i = 0;
-        while (i < N) {
-            if (rows[i] <= i) { ++i; continue; }
+        for (int i = 0; i < N; ++i) {
+            // min zeros at [i]
+            int z = N - i - 1;
+            if (zeros[i] >= z) continue;
             
-            int j = i;
-            while (j + 1 < N) {
-                if (rows[j] <= rows[j + 1]) ++j;
-                else if (rows[j + 1] >= j + 1) ++j;
-                else { 
-                    swap(rows[j], rows[j + 1]); 
-                    ++ans; 
-                    for (int k : rows) cout << k << " "; cout << endl;
-                    break; 
-                }
+            // find zeros[j] >= z
+            int j = i + 1;
+            while (j < N && zeros[j] < z) ++j;
+            if (j >= N) return -1;
+            
+            // swap j to i
+            while (j > i) {
+                swap(zeros[j - 1], zeros[j]);
+                --j;
+                ++ans;
             }
         }
-        
-         cout << endl; for (int k : rows) cout << k << " "; cout << endl;
         return ans;
     }
 };
