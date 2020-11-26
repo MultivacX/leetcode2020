009503745638c5cd091cb252ed8000d256f05135
MultivacX <MultivacX@2020.china>
2020-11-26 23:13:56 +0800

    + hard 887. Super Egg Drop

--- hard/0887. Super Egg Drop.h
+++ hard/0887. Super Egg Drop.h
@@ -1,6 +1,37 @@
 // 887. Super Egg Drop
 // https://leetcode.com/problems/super-egg-drop/
 
+// Runtime: 216 ms, faster than 23.88% of C++ online submissions for Super Egg Drop.
+// Memory Usage: 47.7 MB, less than 7.73% of C++ online submissions for Super Egg Drop.
+
+class Solution {
+public:
+    int superEggDrop(int K, int N) {
+        vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+        // dp[m][k] = max_floors
+        // using m drops & k eggs, we can find F in [1, max_floors] 
+
+        int m = 0;
+        while (dp[m][K] < N) {
+            ++m;
+            for (int k = 1; k <= K; ++k) {
+                // dp[m-1][k-1] : [1, f0] 
+                // dp[m-1][k]   : [1, f1]
+                
+                // drop 1 egg at f0+1
+                // then taking m-1 moves to
+                // if egg broken, using k-1 eggs, can find F in [1, f0] 
+                // if not,        using k eggs,   can find F in [f0+1][f0+2, f0+1+f1] 
+                
+                // dp[m][k]     : [1, f0][f0+1][f0+2, f0+1+f1]
+                dp[m][k] = dp[m - 1][k - 1] + 1 + dp[m - 1][k];
+            }
+        }
+        return m;
+    }
+};
+
+// TLE
 class Solution {
     vector<vector<int>> memo = vector<vector<int>>(101, vector<int>(10001, 20000));
     
