df7b83c8aac5539a456bb470b4d00a6832e497a4
X <>
2020-12-20 17:10:54 +0800

    + medium 277. Find the Celebrity

---
+++ medium/0277. Find the Celebrity.h
@@ -0,0 +1,34 @@
+// 277. Find the Celebrity
+// https://leetcode.com/problems/find-the-celebrity/
+
+// Runtime: 164 ms, faster than 36.51% of C++ online submissions for Find the Celebrity.
+// Memory Usage: 9.8 MB, less than 84.57% of C++ online submissions for Find the Celebrity.
+    
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+    bool knowsSomeone(int n, int x) {
+        for (int i = 0; i < n; ++i)
+            if (x != i && knows(x, i))
+                return true;
+        return false;
+    }
+    
+public:
+    int findCelebrity(int n) {
+        int x = -1;
+        for (int i = 0; i < n; ++i) {
+            if (!knowsSomeone(n, i)) {
+                x = i;
+                break;
+            }
+        }
+        if (x == -1) return -1;
+        
+        for (int i = 0; i < n; ++i) 
+            if (x != i && !knows(i, x))
+                return -1;
+        return x;
+    }
+};
