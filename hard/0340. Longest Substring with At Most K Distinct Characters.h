4d72ebd9f673b69a3bfc6ded68d4c8f831e05f7f
X <>
2020-12-22 16:54:33 +0800

    + hard 340. Longest Substring with At Most K Distinct Characters

---
+++ hard/0340. Longest Substring with At Most K Distinct Characters.h
@@ -0,0 +1,26 @@
+// 340. Longest Substring with At Most K Distinct Characters
+// https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+
+// Runtime: 24 ms, faster than 72.60% of C++ online submissions for Longest Substring with At Most K Distinct Characters.
+// Memory Usage: 8.5 MB, less than 31.30% of C++ online submissions for Longest Substring with At Most K Distinct Characters.
+    
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int ans = 0;
+        unordered_map<char, int> m;
+        for (int i = 0, L = 0; i < s.length(); ++i) {
+            ++m[s[i]];
+            ++L;
+            while (m.size() > k) {
+                char c = s[i + 1 - L];
+                if (--m[c] == 0)
+                    m.erase(c);
+                --L;
+            }
+            if (m.size() <= k && ans < L)
+                ans = L;
+        }
+        return ans;
+    }
+};
