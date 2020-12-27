73755752742ed881f6818b569856a6291cf30e53
X <>
2020-12-27 22:52:59 +0800

    + medium 1698. Number of Distinct Substrings in a String

--- medium/1698. Number of Distinct Substrings in a String.h
+++ medium/1698. Number of Distinct Substrings in a String.h
@@ -1,6 +1,31 @@
 // 1698. Number of Distinct Substrings in a String
 // https://leetcode.com/problems/number-of-distinct-substrings-in-a-string/
 
+// Runtime: 40 ms, faster than 85.71% of C++ online submissions for Number of Distinct Substrings in a String.
+// Memory Usage: 6.8 MB, less than 100.00% of C++ online submissions for Number of Distinct Substrings in a String.
+    
+class Solution {    
+public:
+    int countDistinct(string s) {
+        // s : [0, ..., N-1]
+        //             distinct cnt _ suffix end with s[i]
+        // dp[i] = L : [0, ... , i-L] [i+1-L, ..., i] 
+        const int N = s.length();
+        vector<int> dp(N, 0);
+        for (int l = 1; l <= N; ++l) {
+            for (int i = 0, len = 0; i + l <= N; ++i) {
+                if (s[i] == s[i + l]) ++len;
+                else len = 0;
+                dp[i] = max(dp[i], len);
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < N; ++i) 
+            cnt += i + 1 - dp[i];
+        return cnt;
+    }
+};
+
 // TLE
 class Solution {
     struct Trie {
