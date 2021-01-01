df55c1c4d731fc61d5870c7de09aa726d77c1cf9
X <>
2021-01-01 10:23:30 +0800

    + medium 1057. Campus Bikes

--- medium/1057. Campus Bikes.h
+++ medium/1057. Campus Bikes.h
@@ -1,4 +1,40 @@
 // 1057. Campus Bikes
+// https://leetcode.com/problems/campus-bikes/
+
+// Runtime: 1172 ms, faster than 23.20% of C++ online submissions for Campus Bikes.
+// Memory Usage: 98.2 MB, less than 7.22% of C++ online submissions for Campus Bikes.
+
+class Solution {
+public:
+    vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        const int M = workers.size();
+        const int N = bikes.size();
+        
+        // val: bike_idx * M + worker_idx
+        vector<set<int>> counters(2000);
+        
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                int d = abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1]);
+                counters[d].insert(j * M + i);
+            } 
+        }
+        
+        vector<int> ans(M, -1);
+        vector<bool> assigned(N, false);
+        for (int k = 0, c = 0; k < 2000 && c < M; ++k) {
+            for (auto it : counters[k]) {
+                int i = it % M;
+                int j = it / M;
+                if (ans[i] >= 0 || assigned[j]) continue;
+                ans[i] = j;
+                assigned[j] = true;
+                if (++c >= M) break;
+            }
+        }
+        return ans;
+    }
+};
 
 // TLE
 class Solution {
