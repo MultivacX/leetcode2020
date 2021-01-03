4cd76973c1ebbcc57b238c983af0c70adb3583ec
X <>
2021-01-03 18:34:05 +0800

    + easy 771. Jewels and Stones

---
+++ easy/0771. Jewels and Stones.h
@@ -0,0 +1,17 @@
+// 771. Jewels and Stones
+// https://leetcode.com/problems/jewels-and-stones/
+
+// Runtime: 4 ms, faster than 50.12% of C++ online submissions for Jewels and Stones.
+// Memory Usage: 6.9 MB, less than 16.60% of C++ online submissions for Jewels and Stones.
+    
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> j(begin(jewels), end(jewels));
+        int num = 0;
+        for (char c : stones)
+            if (j.count(c))
+                ++num;
+        return num;
+    }
+};
