ff2a2f2fbfe328bc52c34bbace49ea48e2929cd6
X <>
2021-01-04 11:36:11 +0800

    + medium 1712. Ways to Split Array Into Three Subarrays

--- medium/1712. Ways to Split Array Into Three Subarrays.h
+++ medium/1712. Ways to Split Array Into Three Subarrays.h
@@ -1,37 +1,43 @@
 // 1712. Ways to Split Array Into Three Subarrays
+// https://leetcode.com/problems/ways-to-split-array-into-three-subarrays/
 
+// Runtime: 416 ms, faster than 83.33% of C++ online submissions for Ways to Split Array Into Three Subarrays.
+// Memory Usage: 85.4 MB, less than 66.67% of C++ online submissions for Ways to Split Array Into Three Subarrays.
+    
 class Solution {
 public:
     int waysToSplit(vector<int>& nums) {
         const int N = nums.size();
-        vector<int64_t> pres(N, nums[0]);
+        vector<int> pres(N, nums[0]);
         for (int i = 1; i < N; ++i)
             pres[i] = pres[i - 1] + nums[i];
         
         int64_t ans = 0;
-        for (int i = 0; i < N - 2 && pres[i] * 3 <= pres[N - 1]; ++i) {
-            // l [0, i]
-            
-            // m [i+1, j]
+        for (int i = 0; i < N - 2; ++i) {
+            // [0, i][i+1, j][j+1, N-1]
             // pj - pi >= pi
-            
-            // r [j+1, N-1]
             // pN - pj >= pj - pi
             
-            auto it = lower_bound(begin(pres) + i + 1, end(pres) - 1, 2 * pres[i]);
-            if (it == end(pres) - 1) break;
-            int min_j = it - begin(pres);
+            // 0. left <= mid <= right
+            // 1. left + left <= left + mid
+            // 2. mid * 2 <= mid + right
+            // left <= mid <= (mid + right) / 2
             
-            int64_t half = (pres[N - 1] - pres[i]) / 2;
-            it = lower_bound(begin(pres) + min_j, end(pres), half + pres[i]);
-            if (it == end(pres)) break;
-            int max_j = it - begin(pres);
-            if (pres[N - 1] - pres[max_j] < pres[max_j] - pres[i])
-                --max_j;
+            int left = pres[i];
+            int mid_right = pres[N - 1] - left;
+            if (mid_right < left + left) break;
             
-            if (max_j >= min_j)
-                ans = (ans + max_j - min_j + 1) % 1000000007;
+            // mid
+            // idx : [i+1, N-2]
+            // sum : [i+1, j] ~ [i+1, k]
+            auto b = begin(pres) + i + 1;
+            auto e = end(pres) - 1;
+            int j = lower_bound(b, e, left + left) - begin(pres);
+            int k = upper_bound(b, e, left + mid_right / 2) - begin(pres) - 1;
+            
+            if (k >= j) ans += k - j + 1;
         }
-        return ans;
+        // cout << ans << endl;
+        return ans % 1000000007;
     }
 };
