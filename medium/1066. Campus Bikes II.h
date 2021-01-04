49f959f33f7905b17b306ae9e49a6934f7c02615
X <>
2021-01-04 10:08:44 +0800

    + medium 1066. Campus Bikes II

--- medium/1066. Campus Bikes II.h
+++ medium/1066. Campus Bikes II.h
@@ -1,48 +1,33 @@
+// 1066. Campus Bikes II
+// https://leetcode.com/problems/campus-bikes-ii/
+
+// Runtime: 8 ms, faster than 91.63% of C++ online submissions for Campus Bikes II.
+// Memory Usage: 9.5 MB, less than 70.93% of C++ online submissions for Campus Bikes II.
+    
 class Solution {
     int distance(vector<vector<int>>& workers, vector<vector<int>>& bikes, int i, int j) {
         return abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1]);
     }
     
+    int dfs(vector<vector<int>>& workers, 
+             vector<vector<int>>& bikes, 
+             vector<vector<int>>& memo, 
+             int i, int bikeMask) {
+        if (i >= workers.size()) return 0;
+        if (memo[i][bikeMask] > 0) return memo[i][bikeMask];
+        
+        int ans = INT_MAX;
+        for (int j = 0; j < bikes.size(); ++j) {
+            if (bikeMask & (1 << j)) continue;
+            ans = min(ans, distance(workers, bikes, i, j) + dfs(workers, bikes, memo, i + 1, bikeMask | (1 << j)));
+        }
+        memo[i][bikeMask] = ans;
+        return ans;
+    }
+    
 public:
     int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
-        const int M = workers.size();
-        const int N = bikes.size();
-        
-        unordered_set<int> visited;
-        // cost, i, assigned
-        auto cmp = [](array<int, 3>& a, array<int, 3>& b) { return a[0] > b[0]; };
-        using Q = priority_queue<array<int, 3>, vector<array<int, 3>>, decltype(cmp)>;
-		Q* q = new Q(cmp);
-        q->push({0, 0, 0});
-        int b = 0;
-        while (true) {
-            Q* p = new Q(cmp);
-            while (!q->empty()) {
-                int cost = q->top()[0];
-                int i = q->top()[1];
-                int assigned = q->top()[2];
-                if (!visited.insert((b << 10) | assigned).second)
-                    continue;
-                if (i == M) { delete q; return cost;}
-                q->pop();
-                for (int j = 0; j < N; ++j) {
-                    if (assigned & (1 << j))
-                        continue;
-                    int nc = cost + distance(workers, bikes, i, j);
-                    int ni = i + 1;
-                    int na = assigned | (1 << j);
-                    p->push({nc, ni, na});
-                    
-                    cout << bitset<20>((b << 10) | assigned) << ": ";
-                    cout << nc << ", " << ni << ", " << bitset<10>(na) << endl;
-                }
-                cout << endl;
-            }
-            b = (b << 1) | 1;
-            swap(q, p);
-            delete p;
-        }
-        delete q;
-        return -1;
+        vector<vector<int>> memo(workers.size(), vector<int>(1 << bikes.size(), 0));
+        return dfs(workers, bikes, memo, 0, 0);
     }
 };
