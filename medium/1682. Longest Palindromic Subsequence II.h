924e1dac4e9aa75a37ad57fad74ebc58da50b6d3
X <>
2021-01-06 12:15:26 +0800

    + medium 1682. Longest Palindromic Subsequence II

--- medium/1682. Longest Palindromic Subsequence II.h
+++ medium/1682. Longest Palindromic Subsequence II.h
@@ -1,5 +1,37 @@
 // 1682. Longest Palindromic Subsequence II
-// 
+// https://leetcode.com/problems/longest-palindromic-subsequence-ii/
+
+// Runtime: 788 ms, faster than 33.48% of C++ online submissions for Longest Palindromic Subsequence II.
+// Memory Usage: 258.8 MB, less than 30.40% of C++ online submissions for Longest Palindromic Subsequence II.
+    
+class Solution {
+    int helper(const string& s, vector<vector<vector<int>>>& memo, int i, int j, char pre) {
+        if (i >= j) return 0;
+        
+        int k = pre - 'a';
+        if (memo[i][j][k] >= 0) return memo[i][j][k];
+        
+        int ans = 0;
+        if (s[i] == s[j]) {
+            if (pre == s[i]) {
+                ans = max(helper(s, memo, i + 1, j, pre), helper(s, memo, i, j - 1, pre));
+            } else {
+                ans = 2 + helper(s, memo, i + 1, j - 1, s[i]);   
+            }
+        } else {
+            ans = max(helper(s, memo, i + 1, j, pre), helper(s, memo, i, j - 1, pre));
+        }
+        memo[i][j][k] = ans;
+        return ans;
+    }
+    
+public:
+    int longestPalindromeSubseq(string s) {
+        const int N = s.length();
+        vector<vector<vector<int>>> memo(N, vector<vector<int>>(N, vector<int>(27, -1)));
+        return helper(s, memo, 0, N - 1, 'a' + 26);
+    }
+};
 
 class Solution {
 public:
