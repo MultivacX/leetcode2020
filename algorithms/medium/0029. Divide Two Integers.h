c6640da2acde2e04832d7fc1cd6a00fce6ed39b2
X <>
2021-03-15 16:21:29 +0800

    * Top Interview Questions: medium

--- algorithms/medium/0029. Divide Two Integers.h
+++ algorithms/medium/0029. Divide Two Integers.h
@@ -34,3 +34,51 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int divide(int a, int b) {
+        static const int M = INT_MAX;
+        static const int HM = INT_MAX >> 1;
+        // [-M-1, -M, ..., -1, 0, 1, ..., M]
+        // a / b
+        
+        // 0 / b
+        if (a == 0) return 0;
+        // a / a
+        if (a == b) return 1;
+        
+        // a / 1
+        if (b == 1) return a;
+        // a / -1
+        if (b == -1) return a == -M-1 ? M : -a;
+        
+        // |a| < |b|
+        // a != b & -M <= a <= M
+        if (b == -M-1) return 0;
+        if (a != -M-1 && abs(a) < abs(b)) return 0;
+        
+        // a == -b
+        if (a != -M-1 && abs(a) == abs(b)) return -1;
+        
+        // |a| > |b|
+        int ans = 0;
+        if (a == -M-1) {
+            a = b > 0 ? a + b : a - b;
+            ans = 1;
+        }
+        bool positive = (a > 0 && b > 0) || (a < 0 && b < 0);
+        
+        a = abs(a), b = abs(b);
+        while (a >= b) {
+            int k = 0;
+            while ((b << k) <= HM && (b << (k + 1)) <= a)
+                ++k;
+            a -= b << k;
+            ans += 1 << k;
+        }
+        
+        return positive ? ans : -ans;
+    }
+};
--- algorithms/medium/0166. Fraction to Recurring Decimal.h
+++ algorithms/medium/0166. Fraction to Recurring Decimal.h
@@ -57,3 +57,68 @@ public:
         return integer_part + "." + decimal_part;
     }
 };
+
+
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        int64_t num = numerator, den = denominator;
+        if (num % den == 0) return to_string(num / den);
+        
+        bool positive = (num > 0 && den > 0) || (num < 0 && den < 0);
+        string ans;
+        if (!positive) ans += '-';
+        
+        if (num < 0) num = -num;
+        if (den < 0) den = -den;
+        int k = 0;
+        if (num < den) {
+            while (num < den) num *= 10, ++k;
+            if (k >= 1) ans += "0.";
+            if (k > 1) ans += string(k - 1, '0');
+        }
+        
+        if (num % den == 0) {
+            ans += to_string(num / den);
+        } else {
+            if (k == 0) {
+                int64_t p = num / den;
+                int64_t q = num % den;
+                ans += to_string(p);
+                ans += '.';
+                num = q * 10;
+            }
+            
+            unordered_map<int64_t, int> m;
+            vector<int64_t> v;
+            while (true) {
+                int64_t p = num / den;
+                int64_t q = num % den;
+                
+                m.insert({num, v.size()});
+                v.push_back(p);
+                
+                if (q == 0) {
+                    for (int i : v)
+                        ans += to_string(i);
+                    break;
+                }
+                
+                num = q * 10;
+                
+                if (m.count(num)) {
+                    for (int i = 0; i < m[num]; ++i)
+                        ans += to_string(v[i]);
+                    
+                    ans += '(';
+                    for (int i = m[num]; i < v.size(); ++i)
+                        ans += to_string(v[i]);
+                    ans += ')';
+                    
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
