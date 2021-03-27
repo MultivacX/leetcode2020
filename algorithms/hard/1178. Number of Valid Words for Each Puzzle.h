37576f2b63ce85a6fe155b853201a6570a204f88
X <>
2021-03-27 23:49:05 +0800

    + hard 1178. Number of Valid Words for Each Puzzle

--- algorithms/hard/1178. Number of Valid Words for Each Puzzle.h
+++ algorithms/hard/1178. Number of Valid Words for Each Puzzle.h
@@ -1,6 +1,43 @@
 // 1178. Number of Valid Words for Each Puzzle
 // https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/
 
+// Runtime: 172 ms, faster than 47.15% of C++ online submissions for Number of Valid Words for Each Puzzle.
+// Memory Usage: 35 MB, less than 88.08% of C++ online submissions for Number of Valid Words for Each Puzzle.
+    
+class Solution {
+    int getBits(const string& word) {
+        int bits = 0;
+        for (char c : word)
+            bits |= 1 << (c - 'a');
+        return bits;
+    }
+    
+public:
+    vector<int> findNumOfValidWords(vector<string>& words, vector<string>& puzzles) {
+        unordered_map<int, int> freqs;
+        for (int i = 0; i < words.size(); ++i) 
+            ++freqs[getBits(words[i])];
+        
+        const int n = puzzles.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i) {
+            int first = 1 << (puzzles[i][0] - 'a');
+            int mask = getBits(puzzles[i]);
+            int curr = mask;
+            while (curr) {
+                if (curr & first) {
+                    auto it = freqs.find(curr);
+                    if (it != end(freqs)) 
+                        ans[i] += it->second;
+                }
+                curr = (curr - 1) & mask;
+            }
+        }
+        return ans;
+    }
+};
+
+
 // TLE
 // 9 / 10 test cases passed.
 class Solution {
