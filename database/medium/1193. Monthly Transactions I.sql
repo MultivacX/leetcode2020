bf6f097edace4ed303328d57014b291eaa834579
guo4work <guo4work@outlook.com>
2023-10-27 13:06:48 +0800

    + database medium 1 solved

---
+++ database/medium/1193. Monthly Transactions I.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    to_char(trans_date, 'YYYY-MM') AS month,
+    country,
+    COUNT(state) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY country, month;
